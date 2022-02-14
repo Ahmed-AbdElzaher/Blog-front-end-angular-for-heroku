367142a2b1bf3041c9f850c719624cbbc41e01db
Ahmed Abd-Elzaher <82975877+Ahmed-AbdElzaher@users.noreply.github.com>
2022-02-14 20:34:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.use(express.static(__dirname +'/dist/blog'));
 
-// app.get('/*', (req, res) => {
-//   res.sendFile('index.html',{root: 'dist/blog'});
-// })
+app.get('/*', (req, res) => {
+ res.sendFile('index.html',{root: 'dist/blog'});
+ })
 
 
 app.listen(process.env.PORT || 8000);
